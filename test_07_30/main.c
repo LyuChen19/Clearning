d33a8c82d6bbe176efc9a5656cb5cb2752714c8e
12ttt <108811933+LyuChen19@users.noreply.github.com>
2023-07-31 12:35:38 +0800

    the yest of C lauguage learning

---
+++ test_07_30/main.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+//int main() {
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
+//int main()
+//{
+//    char ch;
+//    while(scanf("%c",&ch)!=EOF)
+//    {
+//
+//    }
+//    return 0;
+//}
+
+//int main()
+//{
+//    int num;
+//    scanf("%d",&num);
+//    if(num % 5 == 0)
+//        printf("YES\n");
+//    else
+//        printf("NO\n");
+//    return 0;
+//}
+
+//int main()
+//{
+//    int i,a;;
+//    while(scanf("%d",&i)!=EOF)
+//    {
+//        for(a=0;a<i;a++)
+//        {
+//            printf("*");
+//
+//        }
+//        printf("\n");
+//    }
+//
+//
+//    return 0;
+//}
+
+//int main()
+//{
+//    int i = 0;
+//    for (i = 0; i<10; i++)
+//    {
+//        if (i = 5)
+//            printf("%d ", i);
+//    }
+//    return 0;
+//}
+
+//写代码将三个整数数按从大到小输出。
+//
+//例如：
+//
+//输入：2 3 1
+//
+//输出：3 2 1
+//
+
+//int main()
+//{
+//    int a,b,c;
+//    scanf("%d %d %d",&a,&b,&c);
+//    if(a>b&&b>c)
+//        printf("%d %d %d",a,b,c);
+//    else if (a>c&&c>b)
+//        printf("%d %d %d",a,c,b);
+//    else if(b>a&&a>c)
+//        printf("%d %d %d",b,a,c);
+//    else if(b>c&&c>a)
+//        printf("%d %d %d",b,c,a);
+//    else if(c>a&&a>b)
+//        printf("%d %d %d",c,a,b);
+//    else
+//        printf("%d %d %d",c,b,a);
+//    return 0;
+//}
+
+//int main()
+//{
+//    int a;
+//    int b;
+//    switch (a)
+//    {
+//        case 1: b = 30;
+//        case 2: b = 20;
+//        case 3: b = 16;
+//        default: b = 0;
+//    }
+//    return b;
+//}
+
+//int main() {
+//    int x = 3;
+//    int y = 3;
+//    switch (x % 2) {
+//        case 1:
+//            switch (y)
+//            {
+//                case 0:
+//                    printf("first");
+//                case 1:
+//                    printf("second");
+//                    break;
+//                default: printf("hello");
+//            }
+//        case 2:
+//            printf("third");
+//    }
+//    return 0;
+//}
